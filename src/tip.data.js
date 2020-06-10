d5ba3a2d015e3dea39b4b0dd340ad71c3479a3c4
Jayden Shaw <jayden.shaw@gmail.com>
2020-06-09 23:54:44 -0600

    Added more data to tip.data

--- src/tip.data.js
+++ src/tip.data.js
@@ -77,6 +77,96 @@ const tip = [
         country: "United States",
         continent: "North America"
     },
+    {
+        title: "Long Lines Building",
+        description: "",
+        country: "United States",
+        continent: "North America"
+    },
+    {
+        title: "Zhangjiajie Glass Bridge",
+        description: "",
+        country: "China",
+        continent: "Asia"
+    },
+    {
+        title: "Deep Throat Parking Garage",
+        description: "",
+        country: "United States",
+        continent: "North America"
+    },
+    {
+        title: "The Imperial War Museum London",
+        description: "",
+        country: "England",
+        continent: "Europe"
+    },
+    {
+        title: "The Sunken City of Baia",
+        description: "",
+        country: "Italy",
+        continent: "Europe"
+    },
+    {
+        title: "The Grolier Club",
+        description: "",
+        country: "United States",
+        continent: "North America"
+    },
+    {
+        title: "Los Jardines de la Tropical",
+        description: "",
+        country: "Cuba",
+        continent: ""
+    },
+    {
+        title: "Colina Kapa Astronomical Observatory",
+        description: "",
+        country: "Bosnia and Herzegovina",
+        continent: "Europe"
+    },
+    {
+        title: "Bibliotheca Philosophica Hermetica - Ritman Library",
+        description: "",
+        country: "Netherlands",
+        continent: "Europe"
+    },
+    {
+        title: "Professional and Amateur Pinball Association World Headquarters",
+        description: "",
+        country: "United States",
+        continent: "North America"
+    },
+    {
+        title: "U.S. Fish and Wildlife Service Forensics Laboratory",
+        description: "",
+        country: "United States",
+        continent: "North America"
+    },
+    {
+        title: '"Singing Runway" at the Abandoned Disney World Airport',
+        description: "",
+        country: "United States",
+        continent: "North America"
+    },
+    {
+        title: "Nishiyama Onsen Keiunkan",
+        description: "",
+        country: "Japan",
+        continent: "Asia"
+    },
+    {
+        title: "Rasputin Museum at Yusupov Palace",
+        description: "",
+        country: "Russia",
+        continent: "Europe"
+    },
+    {
+        title: "Dymaxion Chronofile",
+        description: "",
+        country: "United States",
+        continent: "North America"
+    },
 ];
 
 export default tip;
